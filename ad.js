bf70399a132de1d74da89ce19143ab46ea2a9033
muhamad harith khalid bin walit <harithkhalid93@gmail.com>
2017-06-01 12:13:15 +0800

    Add files via upload

---
+++ ad.js
@@ -0,0 +1,11 @@
+	
+	var adfly_id = 16070915;
+var adfly_advert = 'int';
+var adfly_protocol = 'http';
+var adfly_domain = 'adf.ly';
+var domains = ['#']; 
+var frequency_cap = '5';
+var frequency_delay = '5';
+var init_delay = '3';
+var popunder = true;
+
